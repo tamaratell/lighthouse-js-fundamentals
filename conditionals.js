60781f6d628ac22e70df3c27091d648e116836f8
Tamara Tell <2telltamara@gmail.com>
2023-03-22 20:17:30 +0200

    experimenting with more conditionals

--- conditionals.js
+++ conditionals.js
@@ -19,14 +19,33 @@
 //     console.log("Short sleeves are fine.");
 // }
 
-const temperature = 16;
+// const temperature = 16;
 
-if (temperature < 0) {
-    console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-    console.log("Short sleeves won't cut it!");
-} else {
-    console.log("Short sleeves are fine.");
+// if (temperature < 0) {
+//     console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//     console.log("Short sleeves won't cut it!");
+// } else {
+//     console.log("Short sleeves are fine.");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+    console.log("You are eligible to vote.");
 }
 
-console.log("Now you're ready to go outside!");
+let temperature = 0
+
+if (temperature < -40 || temperature > 40) {
+    console.log("Maybe going outside isn't such a great idea…");
+}
+
+let raining = false
+
+if (!raining) {
+    console.log("Leave your umbrella at home!");
+}
