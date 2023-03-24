5aafd624c46ec897962becdbe77e2e4a4668367d
Tamara Tell <2telltamara@gmail.com>
2023-03-24 18:48:06 +0200

    experimenting with for loops and multiples

---
+++ loopy-lighthouse.js
@@ -0,0 +1,18 @@
+// We will be writing a program that prints the numbers from 100 to 200 to the console, with three exceptions:
+
+// If the number is a multiple of 3, print the string "Loopy" instead of the number.
+// If the number is a multiple of 4, print the string "Lighthouse" instead of the number.
+// If the number is a multiple of both 3 and 4, print the string "LoopyLighthouse" instead of the number.
+// By print, we are referring to console.log.
+
+for (var x = 100; x <= 200; x++) {
+  if (x % 3 === 0 && x % 4 === 0) {
+    console.log("LoopyLighthouse");
+  } else if (x % 3 === 0) {
+    console.log("Loopy");
+  } else if (x % 4 === 0) {
+    console.log("Lighthouse");
+  } else {
+    console.log(x);
+  }
+}
