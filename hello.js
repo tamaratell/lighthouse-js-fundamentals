999002011ec04e8811e76473e3c85becaa34236e
Tamara Tell <2telltamara@gmail.com>
2023-03-27 03:47:52 +0300

    initial save of hello functions

---
+++ hello.js
@@ -0,0 +1,11 @@
+// const sayHello = function () {
+//   console.log("Hello, world");
+// }
+
+// sayHello();
+
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+
+console.log(sayHello("Tamara"));
