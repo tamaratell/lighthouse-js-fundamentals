8a60106ba5cda2609064bff72f30a597036db973
Tamara Tell <2telltamara@gmail.com>
2023-03-27 03:52:24 +0300

    experimenting with functions

--- hello.js
+++ hello.js
@@ -4,8 +4,15 @@
 
 // sayHello();
 
-const sayHello = function (name) {
-  console.log("Hello, " + name);
-}
+// const sayHello = function (name) {
+//   console.log("Hello, " + name);
+// }
 
-console.log(sayHello("Tamara"));
+// console.log(sayHello("Tamara"));
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+console.log(greeting);
