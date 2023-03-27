8c01f422d035e1851ffc8fde968cf6afcdb3eb6c
Tamara Tell <2telltamara@gmail.com>
2023-03-27 13:07:15 +0300

    more javascripting exercises

---
+++ object-properties.js
@@ -0,0 +1,21 @@
+const example = {
+  pizza: 'yummy'
+}
+
+// console.log(example['food'])
+
+const food = {
+  types: 'only pizza'
+}
+
+console.log(food['types']);
+
+// The above code will print the string 'yummy' to the terminal.  
+
+// Alternately, you can use dot notation to get identical results:  
+
+// example.pizza
+
+// example['pizza']
+
+// The two lines of code above will both return yummy.  
